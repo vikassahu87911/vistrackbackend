3ef31bff934239f8df9541b230151a907bd1c5d0
vikassahu87911 <vikassahu1542002@gmail.com>
2026-09-05 01:42:36 +0530

    fixed corsorigin

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const PORT=5000;
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:3000"
-    // origin:"https://viztrack.netlify.app"
+    // origin:"http://localhost:3000"
+    origin:"https://viztrack.netlify.app"
 }))
 
 
